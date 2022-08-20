d2e7d5a86629914f23906e31223e7cd91bfaff4f
junghyeonsu <jung660317@naver.com>
2022-08-20 17:24:50 +0900

    chore: web 프로젝트 생성

---
+++ web/next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+module.exports = withNextra({
+	eslint: {
+    ignoreDuringBuilds: true,
+  },
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko",
+  },
+});
---
+++ web/theme.config.js
@@ -0,0 +1,28 @@
+// theme.config.js
+export default {
+  projectLink: 'https://github.com/junghyeonsu/awesome-dev-activity-review', // GitHub link in the navbar
+  docsRepositoryBase: 'https://github.com/junghyeonsu/awesome-dev-activity-review', // base URL for the docs repository
+  titleSuffix: ' - Awesome-review',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © junghyeonsu.`,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span>Next.js Static Site Generator</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
+  i18n: [{ locale: "ko", text: "한국어" }],
+}
